a6065ff0f9efc78db053c9450bc5854fe46f12ae
sushko14 <sushko14@gmail.com>
2023-11-17 14:24:50 +0200

    add time of device

--- myscript.js
+++ myscript.js
@@ -8,6 +8,7 @@ function clock(){
       hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
       minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
       seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+  document.getElementById('time').innerHTML = hours + ':' + minutes;
   document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
   document.getElementById('month').innerHTML = day + '.' + month;
   document.getElementById('strok').innerHTML = day + '.' + month + '.' + year + ' - ' + day + '.' + month + '.' + year;
